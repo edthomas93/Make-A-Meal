88ed763281fef4d84ce474a5e7751fa783b77e8f
edthomas93 <et1270@my.bristol.ac.uk>
2018-10-11 18:04:59 +0100

    Ingredients can be saved to database via localhost

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 const express = require('express');
-const bodyParser= require('body-parser')
 const app = express();
-// var mongoose = require('mongoose');
-// var db = mongoose.connection;
+const bodyParser= require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://eddyt993:mongopa55word1@ds125713.mlab.com:25713/ingredients";
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.listen(8080, function() {
-  console.log('listening on 8080')
-  app.get('/', (req, res) => {
-    res.sendFile('/Users/edwardthomas/Projects/PostCourse/Make-A-Meal/index.html')
-  })
+MongoClient.connect(url, (err, database) => {
+  if (err) return console.log(err);
 
-  app.post('/next', (req, res) => {
-    console.log(req.body)
+  var db = database.db('ingredients')
+
+  app.listen(8080, () => {
+    console.log('listening on 8080')
+    app.get('/', (req, res) => {
+      res.sendFile('/Users/edwardthomas/Projects/PostCourse/Make-A-Meal/index.html')
+    })
+    app.post('/next', (req, res) => {
+      db.collection('ingredients').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+        console.log('saved to database')
+        res.redirect('/')
+      })
+    })
   })
 })
-
-// mongoose.connect("mongodb://eddyt993:mongopa55word1@ds125713.mlab.com:25713/ingredients", { useNewUrlParser: true });
-// db.on('error', console.error.bind(console, 'Sorry, unable to connect:'));
-// db.once('open', function() {
-//   console.log('Working')
-// });
-//
-// module.exports = mongoose;
