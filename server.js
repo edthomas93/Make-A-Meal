cede878e4befce781aeaffd0dce67fe658cf11e8
edthomas93 <et1270@my.bristol.ac.uk>
2018-10-11 18:45:52 +0100

    Ingredients from database displayed

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://eddyt993:mongopa55word1@ds125713.mlab.com:25713/ingredients";
 
 app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
 
 MongoClient.connect(url, { useNewUrlParser: true }, (err, database) => {
   if (err) return console.log(err);
@@ -14,7 +15,11 @@ MongoClient.connect(url, { useNewUrlParser: true }, (err, database) => {
   app.listen(8080, () => {
     console.log('listening on 8080')
     app.get('/', (req, res) => {
-      res.sendFile('/Users/edwardthomas/Projects/PostCourse/Make-A-Meal/index.html')
+      db.collection('ingredients').find().toArray((err, result) => {
+        if (err) return console.log(err)
+        // renders index.ejs
+        res.render('index.ejs', {ingredients: result})
+      })
     })
     app.post('/next', (req, res) => {
       db.collection('ingredients').insertOne(req.body, (err, result) => {
@@ -23,9 +28,9 @@ MongoClient.connect(url, { useNewUrlParser: true }, (err, database) => {
         res.redirect('/')
       })
     })
-    app.get('/', (req, res) => {
-      var cursor = db.collection('quotes').find()
-      console.log(cursor)
-    })
+    // app.get('/', (req, res) => {
+    //   var cursor = db.collection('quotes').find()
+    //   console.log(cursor)
+    // })
   })
 })
