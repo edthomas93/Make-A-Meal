4455bb802407427e38aa89ec0289cdac20a0e37c
edthomas93 <et1270@my.bristol.ac.uk>
2018-10-13 13:44:31 +0100

    Set up AJAX

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser= require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb://eddyt993:mongopa55word1@ds125713.mlab.com:25713/ingredients";
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const dbUrl = "mongodb://eddyt993:mongopa55word1@ds125713.mlab.com:25713/ingredients";
+const http = new XMLHttpRequest();
+const apiUrl = "https://www.food2fork.com/api/search?key=";
+const API_KEY = "a02f0d0eaec32741c5756fb68f6981de";
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
@@ -10,7 +15,7 @@ app.set('view engine', 'ejs');
 
 var db;
 
-MongoClient.connect(url, { useNewUrlParser: true }, (err, database) => {
+MongoClient.connect(dbUrl, { useNewUrlParser: true }, (err, database) => {
   if (err) return console.log(err)
 
   db = database.db('ingredients')
