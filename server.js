831668efee0d67674707cc2a52b9b134f08e49f1
edthomas93 <et1270@my.bristol.ac.uk>
2018-10-13 13:14:22 +0100

    deleted old files and changed routes

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const bodyParser= require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://eddyt993:mongopa55word1@ds125713.mlab.com:25713/ingredients";
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 MongoClient.connect(url, { useNewUrlParser: true }, (err, database) => {
-  if (err) return console.log(err);
+  if (err) return console.log(err)
 
   var db = database.db('ingredients')
 
@@ -17,20 +18,15 @@ MongoClient.connect(url, { useNewUrlParser: true }, (err, database) => {
     app.get('/', (req, res) => {
       db.collection('ingredients').find().toArray((err, result) => {
         if (err) return console.log(err)
-        // renders index.ejs
         res.render('index.ejs', {ingredients: result})
       })
     })
-    app.post('/next', (req, res) => {
+    app.post('/ingredients/new', (req, res) => {
       db.collection('ingredients').insertOne(req.body, (err, result) => {
         if (err) return console.log(err)
         console.log('saved to database')
         res.redirect('/')
       })
     })
-    // app.get('/', (req, res) => {
-    //   var cursor = db.collection('quotes').find()
-    //   console.log(cursor)
-    // })
   })
 })
