bd501dced499473ebcb53ca43c12514fe0e3a4e7
edthomas93 <et1270@my.bristol.ac.uk>
2018-10-14 13:47:37 +0100

    created getRecipes function

--- routes/recipes.js
+++ routes/recipes.js
@@ -6,14 +6,7 @@ const app_key = "855add853369ee83cc0e41420027dd32";
 const ingredientsModel = require('../models/ingredientsModel')
 
 router.get('/find', (req, res) => {
-  axios.get(`https://api.edamam.com/search?q=chicken&app_id=${app_id}&app_key=${app_key}&from=0&to=3`)
-  .then(function (response) {
-    console.log(response.data.hits);
-    findRecipes();
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  findRecipes();
   res.render('recipes.ejs');
 });
 
@@ -25,7 +18,19 @@ function findRecipes() {
 function iterateIngredients(ingredients) {
   for (key in ingredients){
     console.log(ingredients[key].name)
+    getRecipes();
   }
+}
+
+function getRecipes() {
+  axios.get(`https://api.edamam.com/search?q=chicken&app_id=${app_id}&app_key=${app_key}&from=0&to=3`)
+  .then(function (response) {
+    console.log(response.data.hits);
+    findRecipes();
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 }
 
 module.exports = router;
